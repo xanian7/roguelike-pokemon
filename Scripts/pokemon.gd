9f7c845755cc764ecb4f02e8794d1aa664655d50
xanian7 <xanian407@gmail.com>
2025-03-17 22:08:41 -0400

    adding the names

--- Scripts/pokemon.gd
+++ Scripts/pokemon.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 @onready var sprite_2d = $Sprite2D
 
+@export var pokemon_name: String
+@export var nickname: String
+
 #Sprites
 @export var back_sprite: CompressedTexture2D
 @export var front_sprite: CompressedTexture2D
